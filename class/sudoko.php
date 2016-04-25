04a9b7f49140a4c00000db546199935189bae41e
Omar Vasquez <om.ferito@gmail.com>
2016-04-25 02:13:23 -0500

    documentacion

--- class/sudoko.php
+++ class/sudoko.php
@@ -10,48 +10,54 @@ error_reporting(E_ALL ^ E_NOTICE);
 
  class Sudoku
  {
-     private $dimension_matriz;
-     private $minimoValorAceptado;
-     private $maximoValorAceptado;
+     private $_dimension_matriz;
+     private $_minimoValorAceptado;
+     private $_maximoValorAceptado;
      private $arregloMatriz;
      private $html;
      private $miFuncion = 'sudoku()';
      public  $src_js;
 
+     /**
+      * @params $dim tamaño de la tabla.
+      * @params $min el parametro minimo del de las cajas 
+      * @params $max el parametro maximo del de las cajas 
+      */
      function __construct($dim=2,$min=1,$max=9)
      {
-        # comprovar que halla un dimension_matriz...
-        $this->dimension_matriz = $dim;
-        $this->minimoValorAceptado = $min;
-        $this->maximoValorAceptado = $max;
+        # comprovar que halla un _dimension_matriz...
+        $this->_dimension_matriz = $dim;
+        $this->_minimoValorAceptado = $min;
+        $this->_maximoValorAceptado = $max;
      }
 
      private function createMatriz()
      {
-        for ($i=0; $i < $this->dimension_matriz; $i++)
+        for ($i=0; $i < $this->_dimension_matriz; $i++)
         { 
-            for ($j=0; $j < $this->dimension_matriz ; $j++) 
+            for ($j=0; $j < $this->_dimension_matriz ; $j++) 
             { 
                 #Creamos la matriz con variables aleatorias
-                $matriz[$i][$j]= rand($this->minimoValorAceptado,
-                                             $this->maximoValorAceptado); 
+                $matriz[$i][$j]= rand($this->_minimoValorAceptado,
+                                             $this->_maximoValorAceptado); 
             }
         }#Fin For
         $this->arregloMatriz = $matriz; 
-     }#FcreateMatriz
+     }
 
-     private function CreateHtml($class_table='')
+     /**
+      * @params $class_table referencia a clase css 
+      */
+     private function CreateHtml($class_table)
      {
         //contatenamos para crear la tabla en html 
         $this->html='<table class="'.$class_table.'">';
         foreach ($this->arregloMatriz as $key=>$value) 
         {
-
             $this->html.='<tr>';
             $suma_horizontal=0;
             foreach ($value as $k => $v) 
             {
-
                 $arregloSumavertical[$k] += $v;
                 $suma_horizontal = $suma_horizontal + $v;
                 $op = rand(0,2);
@@ -62,10 +68,8 @@ error_reporting(E_ALL ^ E_NOTICE);
                     $this->html .= $this->input($id_input,$v);
             }
             $this->html .=('<td align="center" id="f'
-                                                 .$key.'">'.$suma_horizontal.'</td>');
-            $this->html .=('</tr>');
+                                                 .$key.'">'.$suma_horizontal.'</td></tr>');
         }
-
         $this->html .=('<tr>');
         foreach ($arregloSumavertical as $key => $value) 
         {
@@ -75,6 +79,10 @@ error_reporting(E_ALL ^ E_NOTICE);
         $this->html.=('</tr></table>');
      }
 
+     /**
+      * @params $id_input referencia del input html
+      * @params $dato boolean disabled input 
+      */
      private function input($id_input='',$dato='')
      {
         $desactivar = (empty($dato)) ? '' : 'disabled' ;
@@ -84,6 +92,9 @@ error_reporting(E_ALL ^ E_NOTICE);
         return $html_input;
      }
 
+     /**
+      * @params $class_table referencia a clase css 
+      */
      public function show($class_table='')
      {
         $this->createMatriz();
@@ -91,9 +102,12 @@ error_reporting(E_ALL ^ E_NOTICE);
         echo $this->html;
      }  
 
+     /**
+      * @params $src_js referencia a script js 
+      */
      public function get_script($src_js=""){
         $js = file_get_contents($src_js);
-        $enviar_dimension_js = str_replace('%dato%',""+$this->dimension_matriz,$js);
+        $enviar_dimension_js = str_replace('%dato%',""+$this->_dimension_matriz,$js);
         echo $enviar_dimension_js;
      }   
 
